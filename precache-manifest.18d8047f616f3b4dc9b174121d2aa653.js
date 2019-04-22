1cef82a279fb94d042dd32ba3e5cf2368d108777
Mikolaj Snochowski <snochopl@gmail.com>
2019-04-22 10:38:55 +0200

    Updates

---
+++ precache-manifest.18d8047f616f3b4dc9b174121d2aa653.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dba14a9f30b654e5c58a",
+    "url": "/snoh666/react-weather-app.git/static/css/main.c95b50ee.chunk.css"
+  },
+  {
+    "revision": "dba14a9f30b654e5c58a",
+    "url": "/snoh666/react-weather-app.git/static/js/main.b0911ae2.chunk.js"
+  },
+  {
+    "revision": "eb1944739dc52a093eda",
+    "url": "/snoh666/react-weather-app.git/static/js/runtime~main.0dc1cfae.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/snoh666/react-weather-app.git/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/snoh666/react-weather-app.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "857a49550ef9b6ba1f7a743aa9b17f28",
+    "url": "/snoh666/react-weather-app.git/index.html"
+  }
+];
