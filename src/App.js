14730b30bea73f3cf6e87b242db43748c8689442
Mikolaj Snochowski <snochopl@gmail.com>
2019-04-22 11:49:40 +0200

    Basic working version

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
 
 const App = () => {
+  const herokuProxy = `http://cors-anywhere.herokuapp.com/`;
+  const [weatherData, setWeatherData] = useState([]);
+  const [refresh, setRefresh] = useState(0);
+  const getRefresh = e => {
+    e.preventDefault();
+    setRefresh(refresh + 1);
+  };
+
+  useEffect(() => {
+    update();
+  }, [refresh]);
+
+  const update = () => {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(data => {
+        fetch(`${herokuProxy}https://api.darksky.net/forecast/56be0ef4bc8f4cdffa9f0de805515c04/${data.coords.latitude},${data.coords.longitude}`)
+          .then(response => {
+            return response.json();
+          }).then(data => {
+            let weatherDataFromFetch = [data.timezone, data.currently.icon, data.currently.temperature + ' F'];
+            setWeatherData(weatherDataFromFetch);
+            console.log(weatherData);
+          }).catch( err => console.log(err));
+      });
+    }
+  };
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <button onClick={getRefresh}>Refresh</button>
+      <div className="app-info">
+        <div className="timezone">{weatherData[0]}</div>
+        <div className="icon">{weatherData[1]}</div>
+      </div>
+      <div className="temperature">{weatherData[2]}</div>
     </div>
   );
 }
